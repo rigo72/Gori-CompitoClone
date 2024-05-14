1114410c94a908ecb45e4e9d39dd352e1242ab54
MattiaGori <mattia.gori27@gmail.com>
2024-05-14 09:44:16 +0200

    errore risolto in stampa

--- stampa.cpp
+++ stampa.cpp
@@ -8,12 +8,12 @@ Gori Mattia 14/05/2024
 using namespace std;
 int main()
 {
-  int i=10 ,n;
+  int i=0 ,n;
   cout<<"Quante volte voi stampare la frase? ";
   cin>>n;
   while (i<n)
   {
-   cout<<"Bada come la fuma"<<endl; 
+   cout<<"Bada come la fuma"<<endl;
+   i++;
   }
-  system("pause");
 }
